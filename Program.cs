1febcf1462b8c0d1f5639e781bd25f553d00e61f
Ivancast67 <166355701+Ivancast67@users.noreply.github.com>
2024-08-23 07:06:36 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numEntero, Cociente, Residuo;
+        Console.Write("\nDigite un número entero: ");
+        numEntero = int.Parse(Console.ReadLine());
+        Cociente = numEntero / 2;
+        Residuo = numEntero - (Cociente * 2);
+
+        if (Residuo == 0)
+        {
+            Console.WriteLine("El número es par");
+        }
+
+        Console.WriteLine("Presione cualquier tecla para continuar...");
+        Console.ReadKey();
+    }
+}
