c78d1f6372638dce9a705eb61584a35fe39b0ee2
Ivancast67 <166355701+Ivancast67@users.noreply.github.com>
2024-08-23 07:12:53 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,2 @@
-﻿using System;
-
-class Program
-{
-    static int a, b;
-
-    static void Main()
-    {
-        Console.Write("Ingrese el primer entero: ");
-        a = int.Parse(Console.ReadLine());
-        Console.Write("Ingrese el segundo entero: ");
-        b = int.Parse(Console.ReadLine());
-        fsuma();
-    }
-
-    static void fsuma()
-    {
-        Console.WriteLine("La suma es igual = " + (a + b));
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
